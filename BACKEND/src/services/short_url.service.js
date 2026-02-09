42d1a82bf5222067512927920eb9666784d77d57
arpit sharma <arpit02456@gmail.com>
2026-02-10 00:53:50 +0530

    fix url normalization usage

--- BACKEND/src/services/short_url.service.js
+++ BACKEND/src/services/short_url.service.js
@@ -1,19 +1,31 @@
 import { generateNanoId } from "../utils/helper.js"
-import urlSchema from "../models/short_url.model.js"
 import { getCustomShortUrl, saveShortUrl } from "../dao/short_url.js"
+
+const normalizeUrl = (url) => {
+    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        return "https://" + url;
+    }
+    return url;
+};
 
 export const createShortUrlWithoutUser = async (url) => {
     const shortUrl = generateNanoId(7)
-    if(!shortUrl) throw new Error("Short URL not generated")
-    await saveShortUrl(shortUrl,url)
+    if (!shortUrl) throw new Error("Short URL not generated")
+
+    const normalizedUrl = normalizeUrl(url)
+
+    await saveShortUrl(shortUrl, normalizedUrl)
     return shortUrl
 }
 
-export const createShortUrlWithUser = async (url,userId,slug=null) => {
+export const createShortUrlWithUser = async (url, userId, slug = null) => {
     const shortUrl = slug || generateNanoId(7)
-    const exists = await getCustomShortUrl(slug)
-    if(exists) throw new Error("This custom url already exists")
 
-    await saveShortUrl(shortUrl,url,userId)
+    const exists = await getCustomShortUrl(slug)
+    if (exists) throw new Error("This custom url already exists")
+
+    const normalizedUrl = normalizeUrl(url)
+
+    await saveShortUrl(shortUrl, normalizedUrl, userId)
     return shortUrl
 }
