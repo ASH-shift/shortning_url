e3afa220b853ae564f7646fe00a71cbf8a527435
arpit sharma <arpit02456@gmail.com>
2026-02-10 00:24:14 +0530

    added url normalization

--- BACKEND/src/dao/short_url.js
+++ BACKEND/src/dao/short_url.js
@@ -2,22 +2,35 @@ import urlSchema from "../models/short_url.model.js";
 import { ConflictError } from "../utils/errorHandler.js";
 
 export const saveShortUrl = async (shortUrl, longUrl, userId) => {
-    try{
+    try {
+
+        // ⭐ FIX: Add protocol automatically
+        if (
+            !longUrl.startsWith("http://") &&
+            !longUrl.startsWith("https://")
+        ) {
+            longUrl = "https://" + longUrl;
+        }
+
         const newUrl = new urlSchema({
-            full_url:longUrl,
-            short_url:shortUrl
-        })
-        if(userId){
-            newUrl.user = userId
+            full_url: longUrl,
+            short_url: shortUrl
+        });
+
+        if (userId) {
+            newUrl.user = userId;
         }
-        await newUrl.save()
-    }catch(err){
-        if(err.code == 11000){
-            throw new ConflictError("Short URL already exists")
+
+        await newUrl.save();
+
+    } catch (err) {
+        if (err.code == 11000) {
+            throw new ConflictError("Short URL already exists");
         }
-        throw new Error(err)
+        throw new Error(err);
     }
 };
+
 
 export const getShortUrl = async (shortUrl) => {
     return await urlSchema.findOneAndUpdate({short_url:shortUrl},{$inc:{clicks:1}});
